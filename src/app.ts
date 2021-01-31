3f250eb2d1e1e16088f3a20cab8edee687925c36
Harun Kilic <harunkilic@live.dk>
2021-01-31 19:52:07 +0100

    Added Cookie-session

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import cookieSession from "cookie-session";
 // TODO: use new package
 import { errorHandler, NotFoundError } from "@udtickets/common";
 
@@ -9,6 +10,13 @@ const app = express();
 // For K8S
 app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    //secure: process.env.NODE_ENV !== "test",
+    secure: false,
+  })
+);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
