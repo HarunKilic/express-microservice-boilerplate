f31718347f85c60bfce1ca735b1016c6f4145f20
Harun Kilic <harunkilic@live.dk>
2021-02-01 10:27:52 +0100

    Update

--- src/index.ts
+++ src/index.ts
@@ -41,6 +41,7 @@ const start = async () => {
     // NATS LISTENERS
 
     // END NATS LISTENERS
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
